65e29eda37304a67baf78d42020fc00f30651510
Jan Nowacki <jasiujanow99@gmail.com>
2020-04-27 13:26:47 +0200

    Dodano ikonę

--- server/index.js
+++ server/index.js
@@ -1,22 +1,22 @@
-const Koa = require('koa')
-const serve = require('koa-static')
-const logger = require('koa-logger')
+const Koa = require("koa");
+const serve = require("koa-static");
+const logger = require("koa-logger");
+const favicon = require("koa-favicon");
 
+const app = new Koa();
+const port = process.env.PORT || 3000;
 
+app.use(serve("client"));
 
-const app = new Koa()
-const port = process.env.PORT || 3000
+app.use(favicon("../client" + "/favicon.ico"));
+app.use(logger());
 
-app.use(serve('client'))
+const userRoutes = require("./routes/users");
+app.use(userRoutes.routes());
 
-app.use(logger())
+const taskRoutes = require("./routes/tasks");
+app.use(taskRoutes.routes());
 
-const userRoutes = require('./routes/users')
-app.use(userRoutes.routes())
+app.listen(port);
 
-const taskRoutes = require('./routes/tasks')
-app.use(taskRoutes.routes())
-
-app.listen(port)
-
-console.log('App is listening at http://127.0.0.1:3000')
+console.log("App is listening at http://127.0.0.1:3000");
