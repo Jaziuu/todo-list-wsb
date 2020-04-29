fc14de4b0610621df271473ea8bed5a36bbd6f45
Jan Nowacki <jasiujanow99@gmail.com>
2020-04-29 16:45:53 +0200

    Dodano ponawianie operacji na tabeli

--- server/store.js
+++ server/store.js
@@ -1,5 +1,7 @@
 const storage = require('azure-storage')
-const service = storage.createTableService()
+const retryOperation = new storage.LinearRetryPolicyFilter();
+const service = storage.createTableService().withFilter(retryOperation);
+
 const table = 'tasks'
 const init = async () => (
     new Promise((resolve, reject) => {
