ba7f94fc42546002adc54c93390702c2b1346aa3
Jan Nowacki <jasiujanow99@gmail.com>
2020-04-30 10:11:08 +0200

    Dodano funkcję dodawania zadań

--- server/index.js
+++ server/index.js
@@ -3,15 +3,21 @@ const serve = require("koa-static");
 const logger = require("koa-logger");
 const favicon = require("koa-favicon");
 require('./store').init()
+const parse = require('koa-bodyparser')
+const store = require('./store')
 
 const app = new Koa();
 const port = process.env.PORT || 3000;
 
+app.use(favicon('./client/favicon.ico'))
+app.use(logger());
 
+store.init();
 app.use(serve("client"));
 
-app.use(favicon("../client" + "/favicon.ico"));
-app.use(logger());
+app.use(parse())
+
+
 
 const userRoutes = require("./routes/users");
 app.use(userRoutes.routes());
--- server/store.js
+++ server/store.js
@@ -1,27 +1,53 @@
+const uuid = require("uuid");
+
 function LoggingFilter() {
     this.handle = (requestOptions, next) => {
         console.log(requestOptions);
         next(requestOptions, (returnObject, finalCallback, next) => {
             console.log(returnObject);
-        })
-    }
+        });
+    };
 }
 
-const storage = require('azure-storage')
+const storage = require("azure-storage");
 const retryOperation = new storage.LinearRetryPolicyFilter();
 const loggingOperation = new LoggingFilter();
-const service = storage.createTableService()
+const service = storage
+    .createTableService()
     .withFilter(loggingOperation)
     .withFilter(retryOperation);
 
-const table = 'tasks'
-const init = async () => (
+const table = "tasks";
+const init = async () =>
     new Promise((resolve, reject) => {
         service.createTableIfNotExists(table, (error, result, response) => {
+            !error ? resolve() : reject();
+        });
+    });
+
+const addTask = async ({
+    title
+}) => (
+    new Promise((resolve, reject) => {
+        const gen = storage.TableUtilities.entityGenerator
+        console.log('addtask - gen')
+        const task = {
+            PartitionKey: gen.String('task'),
+            RowKey: gen.String(uuid.v4()),
+            title
+        }
+        console.log('addtask - task')
+        service.insertEntity(table, task, (error) => {
             !error ? resolve() : reject()
+            if (error) {
+                console.log(error);
+            }
         })
-    })
+        console.log('addtask - insertEntity')
+    }), console.log('addtask - Promise')
 )
+
 module.exports = {
-    init
-}
+    init,
+    addTask
+};
